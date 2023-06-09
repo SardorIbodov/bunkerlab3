432d83a53fdc060647ec7c6283e2f27615d44792
Sardor <sardoribodov07@gmail.com>
2023-06-09 14:19:26 +0500

    Git || GitHub

---
+++ 1/github.js
@@ -0,0 +1,12 @@
+// 1
+// git config --global user.name ""
+// git config --global user.email ""
+
+// 2
+// git init
+
+// 3 => creating origin "folder"
+// git remote add origin "path"
+
+// 4
+// git add .
