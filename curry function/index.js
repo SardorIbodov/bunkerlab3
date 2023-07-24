72cb09eee0ac094f0aa3eb2d3e8594e2f055eac4
Sardor <sardoribodov07@gmail.com>
2023-07-24 15:07:42 +0500

    Updated

---
+++ curry function/index.js
@@ -0,0 +1,9 @@
+//! Curry function
+// function addStrings(str1){
+// 	return function(str2) {
+// 		return function(str3) {
+// 			console.log(str1 + str2 + str3);
+// 		}
+// 	}
+// }
+// addStrings("ja")("va")("script");
